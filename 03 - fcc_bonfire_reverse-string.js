449879d3658e8e8bcb92500f336c4d91009ab12f
Brian Wikene <brianwikene@gmail.com>
2015-07-01 12:09:00 -0700

    Free Code Camp Basic Algorithm Scripting
    bonfires

---
+++ 03 - fcc_bonfire_reverse-string.js
@@ -0,0 +1,10 @@
+function reverseString(str) {
+    var s = '';
+    for (var i = str.length -1; i >=0; i--)
+        s += str[i];
+    return s;
+}
+
+reverseString('hello');
+reverseString('howdy');
+reverseString('Greetings from Earth');
---
+++ 05 - fcc_bonfire_palindrome.js
@@ -0,0 +1,19 @@
+function palindrome(str) {
+
+    var az = str.replace(/\W/g, '');
+    az = az.toLowerCase();
+    console.log(az);
+
+    var za = az.split('').reverse('').join('');
+    console.log(za);
+
+    if (za === az) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+palindrome("eye");
+palindrome("Race! Car!");
+palindrome("A man, a plan, a canal. Panama");
